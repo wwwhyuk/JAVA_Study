76ef062d2676bc7942bfcd56d5996d4fa2e0ce1e
wwwhyuk <joohyeok5118@naver.com>
2023-11-07 16:00:06 +0900

    StaticTest : Static Study

---
+++ JAVA_Study/src/StaticTest/StaticTest.java
@@ -0,0 +1,22 @@
+package StaticTest;
+
+public class StaticTest {
+
+	public static void main(String[] args) {
+		Company man1 = new Company();
+		man1.createMan("이주혁");
+
+		man1.manInfo();
+		
+		Company man2 = new Company();
+		man1.createMan("이주혁");
+
+		man1.manInfo();
+		
+		Company man3 = new Company();
+		man1.createMan("이주혁");
+
+		man1.manInfo();
+	}
+
+}
